60b97ad7f1485b228422ed5e83f54d2ab73bc5c2
Mayank Raj <mayank.raj@e2open.com>
2022-11-22 21:33:31 +0530

    commit after initial completion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Folders/About/about/about.component';
+import { EducationComponent } from './Folders/Education/education/education.component';
+import { ProjectsComponent } from './Folders/PersonalProjects/projects/projects.component';
+import { ProfileComponent } from './Folders/profile/profile.component';
+import { SkillsComponent } from './Folders/skills/skills.component';
+import { ExperienceComponent } from './Folders/WorkExperience/experience/experience.component';
 
-const routes: Routes = [];
+// const routes: Routes = [];
+const routes: Routes = [
+  // {path:'',component: AppComponent, exact: true},
+  { path: 'home',component: ProfileComponent},
+  {path:'about' , component:AboutComponent},
+  {path:'education', component:EducationComponent},
+  {path:'experience', component:ExperienceComponent},
+  {path:'project', component:ProjectsComponent},
+  {path:'skills',component:SkillsComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './Folders/About/about/about.component';
+import { EducationComponent } from './Folders/Education/education/education.component';
+import { HeaderComponent } from './Folders/Header/header/header.component';
+import { ProjectsComponent } from './Folders/PersonalProjects/projects/projects.component';
+import { ExperienceComponent } from './Folders/WorkExperience/experience/experience.component';
+import { ProfileComponent } from './Folders/profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxNavbarModule } from 'ngx-bootstrap-navbar';
+import { SkillsComponent } from './Folders/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    EducationComponent,
+    HeaderComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    ProfileComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxNavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
